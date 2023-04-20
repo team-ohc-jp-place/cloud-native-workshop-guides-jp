a1004a0b0ee3c42334ea19291899a17f36ff6538
Noriaki Mushino <91665878+nmushino@users.noreply.github.com>
2023-04-20 19:01:31 +0900

    Update madtranslate2jp.sh
    2023/6月版

--- madtranslate2jp.sh
+++ madtranslate2jp.sh
@@ -1,4 +1,4 @@
-#!/bin/bash                                                                                                                                                   
+#!/bin/bash                                                                                                                                   
 
 # 引数チェック
 if [ $# != 2 ]; then
@@ -19,21 +19,18 @@ fi
 export MASTER_URL=$1
 oc login ${MASTER_URL} -u admin -p $2 --insecure-skip-tls-verify=true
 
+# ブランチのバージョン
+export CONTENT_PREFIX=main
+
 if [ `oc whoami` ]; then
-  
+
   #ガイドプロジェクトを指定
   oc project dev-guides
   
   #モジュールを日本語に置き換えます 
   for m in {1..5}; do
         echo モジュール $m を置き換えます
-        #export CONTENT_URL_PREFIX=https://raw.githubusercontent.com/team-ohc-jp-place/cloud-native-workshop-v2m${m}-guides/ocp-4.9-jp
-        #export CONTENT_URL_PREFIX=https://raw.githubusercontent.com/nmushino/mad-dev-guides-m${m}-guides/202306-jp
-        #export WORKSHOPS_URLS=${CONTENT_URL_PREFIX}/_cloud-native-workshop-module${m}.yml
         
-        #oc get dc/guides-m${m} -o yaml > orignal_guilde-m${m}.yaml
-        #oc set env dc/guides-m${m} --overwrite CONTENT_URL_PREFIX=${CONTENT_URL_PREFIX} WORKSHOPS_URLS=${WORKSHOPS_URLS}
-
         #英語コンテンツの削除
         oc delete deployment mad-dev-guides-m${m}
         oc delete bc/mad-dev-guides-m${m}
@@ -41,8 +38,8 @@ if [ `oc whoami` ]; then
         oc delete route mad-dev-guides-m${m}
 
         #日本語コンテンツの追加
-        oc new-app https://github.com/team-ohc-jp-place/mad-dev-guides-m${m}.git#main-jp --strategy=docker
+        oc new-app https://github.com/team-ohc-jp-place/mad-dev-guides-m${m}.git#$｛CONTENT_PREFIX｝-jp --strategy=docker
         oc create route edge mad-dev-guides-m${m} --service=mad-dev-guides-m${m}
    done
- 
+   
 fi
